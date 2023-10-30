df591da1d0c336d59d996931e67bac645ef259f1
tomas-fryza <tomas.fryza@gmail.com>
2023-10-30 14:52:00 +0100

    Init comit

---
+++ 01-blink/src/main.cpp
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+
+#define PIN_LED 2
+#define PIN_LED_2 23
+#define PIN_LED_3 22
+
+void setup() {
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+    pinMode(PIN_LED, OUTPUT);
+    pinMode(PIN_LED_2, OUTPUT);
+    pinMode(PIN_LED_3, OUTPUT);
+
+    digitalWrite(PIN_LED, LOW);
+    digitalWrite(PIN_LED_2, LOW);
+    digitalWrite(PIN_LED_3, LOW);
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+    Serial.println("1st LED is on");
+    digitalWrite(PIN_LED, HIGH);
+    delay(500);
+    digitalWrite(PIN_LED, LOW);
+
+    Serial.println("2nd LED is on");
+    digitalWrite(PIN_LED_2, HIGH);
+    delay(500);
+    digitalWrite(PIN_LED_2, LOW);
+
+    Serial.println("3rd LED is on");
+    digitalWrite(PIN_LED_3, HIGH);
+    delay(500);
+    digitalWrite(PIN_LED_3, LOW);
+}
---
+++ 02-timer-blink/src/main.cpp
@@ -0,0 +1,36 @@
+// https://www.upesy.com/blogs/tutorials/timer-esp32-with-arduino-code
+// https://iotespresso.com/timer-interrupts-with-esp32/
+// https://espressif-docs.readthedocs-hosted.com/projects/arduino-esp32/en/latest/api/timer.html
+
+#include <Arduino.h>
+
+#define PIN_LED 2
+
+hw_timer_t * timer = NULL;
+volatile uint8_t led_state = 0;
+
+
+void IRAM_ATTR timer_isr(){
+    led_state = ! led_state;
+    digitalWrite(PIN_LED, led_state);
+}
+
+
+void setup() {
+    uint8_t timer_id = 0;
+    uint16_t prescaler = 80;
+    int threashold = 1000000;
+
+    Serial.begin(115200);
+    pinMode(PIN_LED, OUTPUT);
+
+    timer = timerBegin(timer_id, prescaler, true);
+    timerAttachInterrupt(timer, &timer_isr, true);
+    timerAlarmWrite(timer, threashold, true);
+    timerAlarmEnable(timer);
+}
+
+
+void loop() {
+    // put your main code here to run repeatedly
+}
